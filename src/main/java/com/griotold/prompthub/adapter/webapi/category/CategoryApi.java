7d604ad335c22761e5578c9e8332987e5b7e3fde
griotold <gotjd9773@naver.com>
2025-09-10 12:44:45 +0900

    chore: task_list.md 최신화, 토비님 강의보고 애플리케이션 기능 제공 인터페이스의 리턴 타입 내용을 todo로 작성
    - 애플리케이션 기능 제공 인터페이스의 리턴 타입은 엔티티와 애그리거트, 혹은 그 이상의 연관관계로 애플리케이션 로직 수행 결과를 리턴하기 어려운 경우에만 DTO 클래스/레코드를 만들어 사용한다.

--- src/main/java/com/griotold/prompthub/adapter/webapi/category/CategoryApi.java
+++ src/main/java/com/griotold/prompthub/adapter/webapi/category/CategoryApi.java
@@ -40,6 +40,7 @@ public class CategoryApi {
         return BaseResponse.success(CategoryInfoResponse.of(category));
     }
 
+    // todo 애플리케이션 기능 제공 인터페이스의 리턴 타입은 엔티티와 애그리거트, 예외적으로 연관관계가 복잡한 경우 DTO를 만들어 리턴한다.
     /**
      * 활성 카테고리 목록 조회 (인증 필요)
      */
